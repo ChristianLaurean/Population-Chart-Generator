41ad5bc90fcdb6339e41c0ae1bbf7fde5dce067e
Christian Laurean <christianlaurean1@gmail.com>
2023-02-17 18:12:17 -0600

    Final

--- chart.py
+++ chart.py
@@ -10,7 +10,7 @@ def generate_pie_chart(labels, values):
     fig, ax = plt.subplots()
     ax.pie(values, labels=labels)
     ax.axis('equal')
-    plt.savefig('./imgs/' + 'countries with the largest population.png')
+    plt.savefig('./imgs/' + 'cuntries with the largest population.png')
     plt.close()
 
 
